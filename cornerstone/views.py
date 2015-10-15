f2b2613c5b5d39227610f7e7a526973e13632df4
Anil <anil.kumar@hexalearn.com>
2015-10-15 12:35:28 +0530

    Update views.py

--- cornerstone/views.py
+++ cornerstone/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import render_to_response,render
+from django.shortcuts import render_to_response,render,HttpResponseRedirect
 from .models import CornerstoneUserProfile
 from .forms import CornerstoneUserProfileForm
 
 
-
 def show_csod(request):
-    manager_data = []
     if request.method == 'POST':
-      form = CornerstoneUserProfileForm(request.POST)
-      if form.is_valid():
-         form.save()
-         return render(request, "base.html", {'form': form})
+        form = CornerstoneUserProfileForm(request.POST, request.FILES)
+
+        csvfile = request.FILES['csv_file']
+        file = open(csvfile,'r+')
+        reader = csv.reader(file)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('cornestone.views.show_csod'))
     else:
-       form = CornerstoneUserProfileForm()
+        form = CornerstoneUserProfileForm()
 
     nodes = CornerstoneUserProfile.objects.all()
     return render(request, "base.html", {'form': form, 'nodes': nodes})
